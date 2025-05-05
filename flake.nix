df972366ad7841a333f7e4bb49095056847f8335
jiezhuzzz <jiezzz@duck.com>
2025-05-05 18:06:40 -0500

    set up env

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {
+            inherit system;
+            config = {
+              allowUnfree = true;
+            };
+          };
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default =
+        pkgs.mkShell.override {
+          # Override stdenv in order to change compiler:
+          stdenv = pkgs.llvmPackages_13.stdenv;
+        } {
+          packages = with pkgs;
+            [
+              cmake
+              ninja
+              z3
+              gllvm
+            ]
+            ++ (with pkgs.llvmPackages_13; [
+              llvm
+              clang
+              lld
+            ]);
+
+          # Set LLVM_DIR to point to the LLVM installation
+          shellHook = ''
+            export LLVM_DIR=${pkgs.llvmPackages_13.llvm.dev}/lib/cmake/llvm
+            export LLVM_CONFIG_BINARY=${pkgs.llvmPackages_13.llvm.dev}/bin/llvm-config
+          '';
+        };
+    });
+  };
+}
